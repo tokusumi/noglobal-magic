14fbf65c20742c9e3a0cf3c42b78fc92263a2ed6
tokusumi <tksmtoms@gmail.com>
2021-03-13 16:07:57 +0900

    add off command

--- noglobal_magic/__init__.py
+++ noglobal_magic/__init__.py
@@ -266,11 +266,22 @@ def warn_global(line):
     load_ipython_extension(vw.shell, option=Option.warn_global)
 
 
+@register_line_magic
+def no_global_off(line):
+    unload_ipython_extension(vw.shell)
+
+
+@register_line_magic
+def warn_global_off(line):
+    unload_ipython_extension(vw.shell)
+
+
 if IPYTHON_VER >= version.parse("7.0.0"):
 
     def load_ipython_extension(ip: ip, option: Optional[Option] = None):
         global vw
         if option is None:
+            # at `load_ext noglobal_magic`
             vw = VarWatcher(ip)
             return
         elif option == Option.no_global:
@@ -281,12 +292,22 @@ if IPYTHON_VER >= version.parse("7.0.0"):
         if vw.no_global not in ip.input_transformers_post:
             ip.input_transformers_post.append(vw.no_global)
 
+    def unload_ipython_extension(ip: ip):
+        global vw
+        if vw is None:
+            return
+        if vw.no_global in ip.input_transformers_post:
+            ip.input_transformers_post.remove(vw.no_global)
+        # clear
+        vw = VarWatcher(ip)
+
 
 else:
 
     def load_ipython_extension(ip: ip, option: Optional[Option] = None):
         global vw
         if option is None:
+            # at `load_ext noglobal_magic`
             vw = NoGlobalTransformer(ip)
             return
         elif option == Option.no_global:
@@ -296,3 +317,14 @@ else:
 
         if vw not in ip.input_transformer_manager.python_line_transforms:
             ip.input_transformer_manager.python_line_transforms.append(vw)
+
+    def unload_ipython_extension(ip: ip):
+        global vw
+        if vw is None:
+            return
+
+        if vw in ip.input_transformer_manager.python_line_transforms:
+            ip.input_transformer_manager.python_line_transforms.remove(vw)
+
+        # clear
+        vw = NoGlobalTransformer(ip)
